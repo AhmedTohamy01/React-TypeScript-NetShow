19b8fe20c20c80ec447e5789e73873878ddb0993
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-05 00:10:29 +0200

    feat: build home-page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react"
+import HomePage from './pages/HomePage'
 
 function App() {
-  return <>typescript app</>
+  return <><HomePage /></>
 }
 
 export default App
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function HomePage() {
+  return (
+    <>
+    hello home page
+    </>
+  )
+}
+
+export default HomePage
