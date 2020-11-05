dc69ac969217c0cc81c4917c162a8da739f56baf
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-05 05:16:54 +0200

    feat: build seperator component & add it in home-page

---
+++ src/components/Seperator/Seperator.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from "react"
+import styled from "styled-components"
+
+interface defaultProps {
+  children?: ReactNode
+}
+
+/*---> Component <---*/
+function Seperator({ children, ...restProps }: defaultProps) {
+  return <Wrapper {...restProps}>{children}</Wrapper>
+}
+
+/*---> Styles <---*/
+export const Wrapper = styled.div`
+  border: 4px solid #222;
+`
+
+export default Seperator
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Seperator from '../components/Seperator/Seperator'
 import AccordionCompound from '../compounds/AccordionCompound'
 import HeaderCompound from "../compounds/HeaderCompound"
 import OptFormCompound from '../compounds/OptFormCompound'
@@ -10,6 +11,7 @@ function HomePage() {
 			<HeaderCompound>
 				<OptFormCompound />
 			</HeaderCompound>
+			<Seperator />
 			<AccordionCompound />
     </>
   )
