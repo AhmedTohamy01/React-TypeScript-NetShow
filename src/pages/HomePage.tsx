de719dd3aee72f1e234e1bcb876d271ef3df3d54
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-05 15:58:52 +0200

    refactor: add another seperator after jumbo section in home-page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,19 @@
 import React from "react"
-import Seperator from '../components/Seperator/Seperator'
-import AccordionCompound from '../compounds/AccordionCompound'
+import Seperator from "../components/Seperator/Seperator"
+import AccordionCompound from "../compounds/AccordionCompound"
 import HeaderCompound from "../compounds/HeaderCompound"
-import JumboCompound from '../compounds/JumboCompound'
-import OptFormCompound from '../compounds/OptFormCompound'
-
+import JumboCompound from "../compounds/JumboCompound"
+import OptFormCompound from "../compounds/OptFormCompound"
 
 function HomePage() {
   return (
     <>
-			<HeaderCompound>
-				<OptFormCompound />
-			</HeaderCompound>
-			<Seperator />
-			<JumboCompound />
+      <HeaderCompound>
+        <OptFormCompound />
+      </HeaderCompound>
+      <Seperator />
+      <JumboCompound />
+      <Seperator />
     </>
   )
 }
