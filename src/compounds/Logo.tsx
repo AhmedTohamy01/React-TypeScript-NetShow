c966dedff31aae51d56068d1994de3e6d76733fa
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-05 02:01:34 +0200

    feat: build logo component

---
+++ src/compounds/Logo.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode } from "react"
+import styled from "styled-components"
+
+interface defaultProps {
+  children?: ReactNode
+}
+
+/*---> Component <---*/
+function Logo({ children, ...restProps }: defaultProps) {
+  return (
+    <div>
+      <a href="/" {...restProps}>
+        {children}
+        <Image
+          className="logo"
+          src="/images/misc/logo.svg"
+          alt="Netflix logo"
+        />
+      </a>
+    </div>
+  )
+}
+
+/*---> Styles <---*/
+export const Image = styled.img`
+  height: 32px;
+  width: 108px;
+`
+
+export default Logo
