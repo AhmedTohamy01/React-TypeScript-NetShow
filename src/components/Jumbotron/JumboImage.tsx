7c7b611c82e3f146dfe9fa2cdc09f5ed1a9ac1eb
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-05 05:35:05 +0200

    feat: build jumbo-image component

---
+++ src/components/Jumbotron/JumboImage.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from "react"
+import styled from "styled-components"
+
+interface defaultProps {
+  children?: ReactNode
+}
+
+/*---> Component <---*/
+function JumboImage({ children, ...restProps }: defaultProps) {
+  return <Image {...restProps}>{children}</Image>
+}
+
+/*---> Styles <---*/
+export const Image = styled.img`
+  max-width: 100%;
+  height: auto;
+`
+
+export default JumboImage
