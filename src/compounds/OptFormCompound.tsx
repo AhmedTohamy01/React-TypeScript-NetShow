bb54248e41797aadcc5f5bd9fb59f76317bf759a
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-05 05:10:51 +0200

    feat: build optform-button & add it in optform-compound

--- src/compounds/OptFormCompound.tsx
+++ src/compounds/OptFormCompound.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import OptFormButton from '../components/OptForm/OptFormButton'
 import OptFormEmail from '../components/OptForm/OptFormEmail'
 import OptFormText from '../components/OptForm/OptFormText'
 import OptFormWrapper from '../components/OptForm/OptFormWrapper'
@@ -10,9 +11,10 @@ function OptFormCompound() {
       <OptFormText>
         Ready to watch? Enter your email to create or restart your membership.
       </OptFormText>
-			<OptFormWrapper>
-				<OptFormEmail />
-			</OptFormWrapper>
+      <OptFormWrapper>
+        <OptFormEmail />
+        <OptFormButton>Get Started</OptFormButton>
+      </OptFormWrapper>
     </>
   )
 }
