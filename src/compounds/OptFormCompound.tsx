f70dc54f525ec97df1d246de0e41219bc19db4ba
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-05 05:07:32 +0200

    refactor: move optfrom-text out of optform-wrapper

--- src/compounds/OptFormCompound.tsx
+++ src/compounds/OptFormCompound.tsx
@@ -6,11 +6,12 @@ import OptFormWrapper from '../components/OptForm/OptFormWrapper'
 function OptFormCompound() {
   return (
     <>
-      <OptFormWrapper>
-        <OptFormText>
-          Ready to watch? Enter your email to create or restart your membership.
-        </OptFormText>
-      </OptFormWrapper>
+      <OptFormText>
+        Ready to watch? Enter your email to create or restart your membership.
+      </OptFormText>
+			<OptFormWrapper>
+				
+			</OptFormWrapper>
     </>
   )
 }
