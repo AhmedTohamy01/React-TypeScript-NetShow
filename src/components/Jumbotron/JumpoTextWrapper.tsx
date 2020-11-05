9b0de6bb0bc033c4028c10bd72fca38b25d1fa15
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-05 05:44:52 +0200

    feat: build jumbo-text-wrapper component

---
+++ src/components/Jumbotron/JumpoTextWrapper.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from "react"
+import styled from "styled-components"
+
+interface defaultProps {
+  children?: ReactNode
+}
+
+/*---> Component <---*/
+function JumpoTextWrapper({ children, ...restProps }: defaultProps) {
+  return <Wrapper {...restProps}>{children}</Wrapper>
+}
+
+/*---> Styles <---*/
+export const Wrapper = styled.div``
+
+export default JumpoTextWrapper
