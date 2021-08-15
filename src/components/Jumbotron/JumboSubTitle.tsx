216d6b550a6e7b104a623a278b1902bc13e15a78
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-08-16 01:21:45 +0200

    refactor: refactor JumboSubTitle component

--- src/components/Jumbotron/JumboSubTitle.tsx
+++ src/components/Jumbotron/JumboSubTitle.tsx
@@ -1,13 +1,13 @@
 import React, { ReactNode } from 'react'
 import styled from 'styled-components'
 
-interface defaultProps {
-  children?: ReactNode
+interface ChildrenType {
+  children?: string
 }
 
 /*---> Component <---*/
-const JumboSubTitle = ({ children, ...restProps }: defaultProps) => {
-  return <SubTitle {...restProps}>{children}</SubTitle>
+const JumboSubTitle = ({ children }: ChildrenType) => {
+  return <SubTitle>{children}</SubTitle>
 }
 
 /*---> Styles <---*/
