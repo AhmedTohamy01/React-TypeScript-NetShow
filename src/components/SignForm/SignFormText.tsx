dada506a7f3765c77dd124e9709dcb93164a8701
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-08-21 23:04:57 +0200

    feat: add SignFormText component

---
+++ src/components/SignForm/SignFormText.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface PropsType {
+  children?: string
+}
+
+/*---> Component <---*/
+function SignFormText({ children }: PropsType) {
+  return <Text>{children}</Text>
+}
+
+/*---> Styles <---*/
+export const Text = styled.p`
+  color: #8c8c8c;
+  font-size: 16px;
+  font-weight: 500;
+`
+
+export default SignFormText
