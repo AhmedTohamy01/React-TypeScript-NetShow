39d9eb931d85d8c7e00e1fcb5c136f70a23045c8
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-08-22 01:06:37 +0200

    feat: add more props to SignFormText component

--- src/components/SignForm/SignFormText.tsx
+++ src/components/SignForm/SignFormText.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import styled from 'styled-components'
 
 interface PropsType {
-  children?: string
+  children?: string | ReactNode
 }
 
 /*---> Component <---*/
