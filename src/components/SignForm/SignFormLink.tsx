7bb85c162e3d0bf0dcdb7c04a9c2250200ec2b9f
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-08-21 23:24:35 +0200

    feat: add SignFormLink component

---
+++ src/components/SignForm/SignFormLink.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface PropsType {
+  children?: string
+}
+
+
+/*---> Component <---*/
+function SignFormLink({ children }: PropsType) {
+  return <Link>{children}</Link>
+}
+
+/*---> Styles <---*/
+export const Link = styled.a`
+  color: #fff;
+  text-decoration: none;
+  margin-right: 30px;
+  cursor: pointer;
+  text-decoration: none;
+  margin-left: 5px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+export default SignFormLink
