fe5ee577b65ae6c2ca165ecaa9c4a8914842722a
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-08-21 22:58:29 +0200

    feat: add SignFormError component

---
+++ src/components/SignForm/SignFormError.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface PropsType {
+  children?: string
+}
+
+/*---> Component <---*/
+function SignFormWrapper({ children }: PropsType) {
+  return <Wrapper>{children}</Wrapper>
+}
+
+/*---> Styles <---*/
+export const Wrapper = styled.div`
+  background: #e87c03;
+  border-radius: 4px;
+  font-size: 14px;
+  margin: 0 0 16px;
+  color: white;
+  padding: 15px 20px;
+`
+
+export default SignFormWrapper
