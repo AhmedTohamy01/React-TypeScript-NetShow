26fa6391fad7376689d59fe75596ca02c84bdc50
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-08-22 17:07:48 +0200

    feat: add route for SigninPage n App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import HomePage from './pages/HomePage'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import SigninPage from './pages/SigninPage'
 
 const App = () => {
   return (
-    <>
-      <HomePage />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+        <Route path='/signin'>
+          <SigninPage />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
