0966789a8e08c988934cec5d6a67600836eee29d
Ahmed Tohamy <ahmed-tohamy@outlook.com>
2021-11-29 04:14:22 +0200

    feat: protect browse page with private component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HomePage from './pages/HomePage'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import SigninPage from './pages/SigninPage'
 import SignupPage from './pages/SignupPage'
 import BrowsePage from './pages/BrowsePage'
 import NotFoundPage from './pages/NotFoundPage'
+import PrivateRoute from './components/PrivateRoute/PrivateRoute'
+import firebase from '../src/lib/firebase.prod'
 
 /*---> Component <---*/
 const App = () => {
+  const [authData, setAuthData] = useState(false)
+  console.log('authData', authData)
+  useEffect(() => {
+    const getAuthData = () => {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          setAuthData(true)
+        }
+      })
+    }
+    getAuthData()
+  }, [])
+
   return (
     <Router>
       <Switch>
@@ -20,9 +35,9 @@ const App = () => {
         <Route path='/signup'>
           <SignupPage />
         </Route>
-        <Route path='/browse'>
+        <PrivateRoute path='/browse' authData={authData}>
           <BrowsePage />
-        </Route>
+        </PrivateRoute>
         <Route path='*'>
           <NotFoundPage />
         </Route>
